e87c2e6e29065881e83406b06842dc932c3695b6
kdouglass805 <kdouglass805@boyslatin.net>
2026-09-01 13:37:01 +0000

    class notes

---
+++ datavtypes.py
@@ -0,0 +1,41 @@
+# STRING- characters that are wrapped whithin quotes.
+# charatcters can be letters, numbers, and symbols.
+# you can use both simgle and double quotation
+# marks for string
+"Ian Kimble"
+
+
+'123 Main Street'
+
+' This cost $100 dollars'
+
+# the hashtag is a marker that makes anything written
+# behind it invisible to the computer
+
+# INTEGERS - datatypes that are just whole numbers, or numbers
+# without a decimal point. 
+# we dont have to add any special symbols. we can write the number
+# as is. 
+
+10
+
+20
+
+2000000
+
+# FLOATS- A number with a decimal. 
+
+20.99
+
+3.339
+
+0.232
+
+
+# BOOLEAN- a data type that can only be either true or false
+# we do not use quotation marks for boolean values. the computer 
+# understand these words as something unique.
+
+False
+
+True
