5e5e34f48a7abb4c36987763548e2e98648b6255
kdouglass805 <kdouglass805@boyslatin.net>
2026-09-03 13:45:12 +0000

    notes

---
+++ Demo.py
@@ -0,0 +1,5 @@
+
+name - Kaden
+grade level - 10th
+favorite class - English 
+hello good morning 
---
+++ variableActivity.py
@@ -0,0 +1,45 @@
+Variables- a Container that stores data. you can
+# name you container almost anything you want, so long
+# as it is descriptive 
+
+name= "Kaden Douglass"
+yearsofex= 1000
+graduate= True
+
+#When we want to create a variable, we make up a word
+# use the equal sign and the assign it a data type.
+# whenever the computer sees our variabale word it will
+# always use the data indside.
+
+
+
+
+#Variable naming rules
+#1. A variable MUST start with letter and cannot start with a number 
+#2. A variable can be a mixs of letters and numbers BUT not symbols
+#3. A variable CANNOT be a Phython keyword
+
+
+
+# Class Assignment
+ Applenumbermarket=500
+studentsmathclass=32
+tripinsurance=3000
+membershipprices=500/600/700
+drinksthree=5
+
+#Variable Naming Conventions
+# We used naming conventions because we CANNOT have spaces in
+# variable names
+
+
+# Camel case - when creating a new word or new variable names with multiple words,
+# every new word begins with capital letter
+# the reason why its camel case is bc every new word 
+# represents a hump.
+
+
+# Snake case _ when creating a variable using snake case
+# we use a underscore symbol to space out each new word
+# the reason why its called snack case is bc the underscore
+# represents a snack on the ground 
