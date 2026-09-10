d0f635cd4b3c38d0f9791529a1f5acb76747d177
kdouglass805 <kdouglass805@boyslatin.net>
2026-09-10 13:39:16 +0000

    operators notes

--- Unit 1/operators.py
+++ Unit 1/operators.py
@@ -20,7 +20,7 @@ print(4 - 4) # subtraction operator
 print( 12/ 4) # division operator
 print (4 * 4 ) # multiplication operator
 
-# Comparison Operators - set of symbols used]
+# Comparison Operators - set of symbols used
 # assess if data is the same or different and
 # how they differ
 
@@ -28,4 +28,30 @@ print(10 > 1) # greater than operator
 print(10 < 1) # less than operator
 
 # 2 equal signs compare if something is the same
-print("kaden" == "Kaden") # same as
+print("kaden" == "Kaden") # same as (false)
+print("2" == 2) # same as (false) not the same datatypes
+print(2.0 == 2) # same as (true)
+
+
+# not equal is written with !=
+# this is to check and filter for vaules that are not
+# the same
+print(200 != 100) # True- these are not the same
+print(300 != 300) # these are the same\
+
+# logical operators- compares 2 conditions to check if 
+# they are true or false
+
+# AND- checks if 2 conditions are true. if yes, the final
+# result if true
+print(3 < 1 and 100 > 50) # this would come out to be true
+
+# OR- checks if omly 1 condition is true. if yes ,
+# the final result will be true
+print( 3 > 1 or 100 > 50)
+
+
+# NOT - "the opposite day". it will reserve the 
+# result of the logical operators
+print(not(3 > 1 and 100 > 50))
+#this would come out to be false
